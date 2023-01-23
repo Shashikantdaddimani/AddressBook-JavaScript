f0088e49d48e31858da4f54a0739a3fbceebf48d
Shashikantdaddimani <shashi0344daddimani@gmail.com>
2023-01-23 13:12:39 +0530

    Added:Logic for Finding the existing contact person using theirname and editing the contact details

--- AddressBook_Programs/AddressBook.js
+++ AddressBook_Programs/AddressBook.js
@@ -1,6 +1,8 @@
-class ContactDetails {
+
+
+class ContactDetails{
     /**
-     * In this class create contact details as data members
+     * Contact details of addressBook
      */
     firstName;
     lastName;
@@ -9,11 +11,11 @@ class ContactDetails {
     state;
     zipCode;
     phoneNumber;
-    emailID;
+    emailId;
     /**
-     *  peremeterized constructor
+     * Prameterized Constructor
      */
-    ContactDetails(firstName, lastName, address, city, state, zipCode, phoneNumber, emailID) {
+    ContactDetails(firstName, lastName, address, city, state, zip, phoneNumber, email) {
         this.firstName = firstName;
         this.lastName = lastName;
         this.address = address;
@@ -21,118 +23,137 @@ class ContactDetails {
         this.state = state;
         this.zipCode = zipCode;
         this.phoneNumber = phoneNumber;
-        this.emailID = emailID;
+        this.emailId = emailId;
     }
     /**
      * default constructor
      */
-    ContactDetails() {
+   ContactDetails(){
 
     }
-    /**
-     * toString Method
-     */
     toString() {
-        return "First Name : " + this.firstName + "Last Name : " + this.lastName + "Address : " + this.address + "City : " + this.city + "State : " + this.state + "zip code : " + this.zipCode + "Phone Number : " + this.phoneNumber + "emailID : " + this.emailID;
+        return "First Name : " + this.firstName + ", Last Name : " + this.lastName + ", Address : " + this.address + ", City : " + this.city + ", State : " + this.state + ", zip code : " + this.zipCode + ", Phone Number : " + this.phoneNumber + ", emailId : " + this.emailId;
     }
 }
 /**
- * Create Array and ContactDetails Objects
+ * This Function Validate the FirstName And LastName
  */
-contactList = new Array();
-contact = new ContactDetails();
-function addDetails() {
-    /**
-     * In this method Ability to add contact details to address book
-     */
-    contact.firstName = firstName('first name');
-    contact.lastName = firstName('last name');
+function firstName(value){
+   let pattern = new RegExp("^[A-Z]{1}[a-z]{2,}$");
+   let input;
+   while(true){
+    input=prompt("Enter"+value+"(First Latter is UpperCase)");
+    let status=pattern.test(input);
+    if(status){
+       break; 
+    }  
+   }
+   return input;
+}
+/**
+ * This function is Validate the Address,city,state.
+ */
+function address(value){
+    let pattern = new RegExp("^[A-Z]{1}[a-z]{2,}$")
+    let input;
+    while(true){
+        input=prompt("Enter"+value+"(First letter is UpperCase )");
+        let status=pattern.test(input);
+        if(status){
+            break;
+        }
+    }
+    return input;
+}
+
+/**
+ * This function is Validate the ZipCode
+ */
+function zipCode(){
+    let pattern =new RegExp("^[1-9]{1}[0-9]{5}$");
+    let input;
+    while(true){
+        input=prompt("Enter Zip code (Zip code Contains 6 digits");
+        let status = pattern.test(input);
+        if(status){
+            break;
+        }
+    }
+    return input;
+}
+/**
+ * This function is Validate the PhoneNumber
+ */
+function phoneNumber(){
+    let pattern = new RegExp("^[6-9]{1}[0-9]{9}$");
+    let input;
+    while(true){
+        input = prompt("Enter PhoneNumber (The Mobile Number contains ten digits and First digit contain inbetween 6-9 digit: )");
+        let status = pattern.test(input);
+        if(status){
+            break;
+        }
+    }
+    return input;
+}
+/**
+ * This function is Validate the EmailId
+ */
+function emailId(value){
+    let pattern = new RegExp("^[A-Za-z]{3,20}[0-9]{0,10}[@]{1}[gmail.com]{9}$");
+    let input;
+    while(true){
+        input=prompt("Enter EmailId")
+        let status =pattern.test(input);
+        if(status){
+            break;
+        }
+    }
+    return input;
+}
+contactList =new Array();
+/**
+ * Method for adding the contact Details
+ */
+function addContactDetails(){
+    contact = new ContactDetails();
+    contact.firstName =firstName("FirstName");
+    contact.lastName=firstName("LastName");
     contact.address = address('Address');
     contact.city = address('city');
     contact.state = address('State');
     contact.zipCode = zipCode();
     contact.phoneNumber = phoneNumber();
-    contact.emailID = emailID();
-    contactList.push(contact)
+    contact.emailId = emailId();
+    contactList.push(contact);
+
 }
-function printDetails() {
-    /**
-     * In this method print the Address book contact details
-     */
+/**
+ * function for Printing the Contact details
+ */
+function printContactDetails() {
+   
     console.log(contactList);
 }
-function firstName(value) {
-    /**
-     * In this method validate the first name and last name
-     */
-    let pattern = new RegExp('^[A-Z]{1}[a-z]{2,}$');
-    let input;
-    while (true) {
-        input = prompt("Enter " + value + " (first letter is upper case)");
-        let status = pattern.test(input);
-        if (status) {
+/**
+ * function for editing the contact details 
+ */
+function editContactDetails() {
+    
+    let input = prompt("Enter first name");
+    let status = true;
+    for (let i = 0; i < contactList.length; i++) {
+        if (contactList[i].firstName == input) {
+            contactList.splice(i, 1);
+            status = false;
             break;
         }
     }
-    return input;
-}
-function address(value) {
-    /**
-     * In this method validate the address, city and state
-     */
-    let pattern = new RegExp('^[A-Z]{1}[a-z]{2,}$');
-    let input;
-    while (true) {
-        input = prompt("Enter " + value + " (first letter is upper case)");
-        let status = pattern.test(input);
-        if (status) {
-            break;
-        }
+    if (status == true) {
+        console.log(input + " details not found");
     }
-    return input;
-}
-function zipCode() {
-    /**
-     * In this method validate the zip code
-     */
-    let pattern = new RegExp('^[0-9]{6}$');
-    let input;
-    while (true) {
-        input = prompt("Enter zip code (Enter 6 numbers)");
-        let status = pattern.test(input);
-        if (status) {
-            break;
-        }
+    else {
+        addContactDetails();
     }
-    return input;
-}
-function phoneNumber() {
-    /**
-     * In this method validate the mobile number
-     */
-    let pattern = new RegExp('^[7-9]{1}[0-9]{9}$');
-    let input;
-    while (true) {
-        input = prompt("Enter Mobile Number (Enter 9 numbers)");
-        let status = pattern.test(input);
-        if (status) {
-            break;
-        }
-    }
-    return input;
-}
-function emailID() {
-    /**
-     * In this method validate the email
-     */
-    let pattern = new RegExp('^[A-Za-z]{3,20}[0-9]{0,10}[@]{1}[gmail.com]{9}$');
-    let input;
-    while (true) {
-        input = prompt("Enter email ");
-        let status = pattern.test(input);
-        if (status) {
-            break;
-        }
-    }
-    return input;
+
 }
